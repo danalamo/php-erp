3184795335aa7105ddfc5e06b11a1b8e1789e785
Daniel Alamo <daniel.alamo@gmail.com>
2020-02-05 21:10:43 -0500

    Improve table styling

--- index.php
+++ index.php
@@ -8,6 +8,26 @@ $data = [
 
 render($data, function($data) {
 ?>
+    <table class="stripped">
+        <thead>
+            <tr>
+                <th>Active</th>
+                <th>Employee Name</th>
+                <th>Location</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach($data['users'] as $user): ?>
+            <tr>
+                <td><?= $user->active ? 'true' : 'false' ?></td>
+                <td><?= "{$user->first_name} {$user->last_name}" ?></td>
+                <td><?= $user->email ?></td>
+                <td><?= 'Edit | Delete' ?></td>
+            </tr>
+        <?php endforeach ?>
+        </tbody>
+    </table>  
 <?php
     dd(relative_path(__FILE__));
 });
