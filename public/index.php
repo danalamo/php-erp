c71409ea16648890ed67e342532fa5b9a7136aae
Daniel Alamo <daniel.alamo@gmail.com>
2020-02-06 19:28:20 -0500

    Improve `Employee Listing` table

--- public/index.php
+++ public/index.php
@@ -14,26 +14,36 @@ try {
 $data = [
     'users' => $users,
 ];
+$data['page_title'] = 'Employee Directory';
 
 render($data, function($data) {
 ?>
-    <table class="stripped">
-        <thead>
-            <tr>
-                <th>Active</th>
-                <th>Employee Name</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach($data['users'] as $user): ?>
-            <tr>
-                <td><?= $user->active ? 'true' : 'false' ?></td>
-                <td><?= "{$user->first_name} {$user->last_name}" ?></td>
-                <td><?= 'Edit | Delete' ?></td>
-            </tr>
-        <?php endforeach ?>
-        </tbody>
-    </table>
+    <div class="table responsive">
+        <table class="stripped">
+            <thead>
+                <tr>
+                    <th>Active</th>
+                    <th>Employee Name</th>
+                    <th>Location</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($data['users'] as $user): ?>
+                <tr 
+                    <?= $user->active ? '' : ' class="user-inactive" ' ?>
+                >
+                    <td><?= $user->active ? 'Yes' : 'No' ?></td>
+                    <td><?= _esc("{$user->last_name}, {$user->first_name}") ?></td>
+                    <td><?= _esc("$user->line1; $user->city, $user->state $user->zip") ?></td>
+                    <td>
+                        <a href="/employees/edit.php?user_id=<?= $user->id ?>">edit</a>
+                        | <a href="/employees/delete.php?user_id=<?= $user->id ?>">delete</a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
 <?php
 });
